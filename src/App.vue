1325351d83735e909140dfe548188606339fd641
Filip Krunic <fkrunic9@gmail.com>
2023-09-06 12:54:27 -0700

    basic mobile structure

--- src/App.vue
+++ src/App.vue
@@ -7,9 +7,13 @@ onMounted(() => {
 </script>
 
 <template>
-  <div class="flex items-center justify-center min-h-screen">
-    <p class="text-3xl font-bold underline">
-      Hello world!
-    </p>
+  <div class="flex flex-col items-center justify-center min-h-screen">
+    <p>PING.</p>
+    <p>We are launching <span>soon!</span></p>
+    <p>Subscribe and get notified</p>
+    <input type="text" placeholder="Your email address...">
+    <p>Notify Me</p>
+    <img src="./assets/illustration-dashboard.png">
+    <p>&#169; Copyright Ping. All rights reserved.</p>
   </div>
 </template>
