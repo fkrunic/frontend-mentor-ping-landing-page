11531efd94676a02f55a5fbdfe84f0decf791d5d
Filip Krunic <fkrunic9@gmail.com>
2023-09-06 13:27:51 -0700

    email states

--- src/App.vue
+++ src/App.vue
@@ -1,5 +1,6 @@
 <script setup lang="ts">
 import { onMounted } from 'vue';
+import EmailEntry from './components/EmailEntry.vue'
 
 onMounted(() => {
   document.body.className = 'font-libre-franklin'
@@ -13,10 +14,7 @@ onMounted(() => {
       <p class="text-2xl text-gray">We are launching <span class="font-bold text-black">soon!</span></p>
       <p class="text-sm">Subscribe and get notified</p>
     </div>
-    <div class="flex flex-col gap-3 items-center w-full">
-      <input class="w-full rounded-full text-sm placeholder-pale-blue outline-none border-[1px] border-solid border-pale-blue p-4 pl-8" type="text" placeholder="Your email address...">            
-      <p class="bg-blue text-white text-sm w-full rounded-full p-4 text-center font-semibold shadow-xl cursor-pointer shadow-blue/25">Notify Me</p>
-    </div>
+    <EmailEntry></EmailEntry>
     <img src="./assets/illustration-dashboard.png">
     <p class="absolute bottom-6 w-full text-center text-xs text-gray">&#169; Copyright Ping. All rights reserved.</p>
   </div>
---
+++ src/components/EmailEntry.vue
@@ -0,0 +1,59 @@
+<script setup lang="ts">
+import { match } from 'ts-pattern'
+import { ref } from 'vue';
+
+type EmailState
+  = { kind: 'empty' }
+  | { kind: 'valid' }
+  | { kind: 'invalid' }
+
+const emailState = ref({ kind: 'empty'} as EmailState)
+
+const borderColor = (es: EmailState): string => {
+  return match(es)
+    .with({ kind: 'empty'}, () => 'border-pale-blue')
+    .with({ kind: 'valid'}, () => 'border-pale-blue')
+    .with({ kind: 'invalid'}, () => 'border-red')
+    .exhaustive()
+}
+
+</script>
+
+<template>
+  <div class="flex flex-col gap-3 items-center w-full">
+    <input class="
+      w-full 
+      p-4 
+      pl-8
+
+      text-sm 
+      
+      outline-none 
+      border-[1px] 
+      border-solid 
+      rounded-full 
+      
+      placeholder-pale-blue" 
+      :class="borderColor(emailState)"
+      type="text" 
+      placeholder="Your email address..."
+      >    
+    <p class="
+      w-full
+      p-4
+
+      bg-blue 
+      text-white 
+      text-sm  
+      
+      rounded-full  
+      shadow-xl
+      shadow-blue/25
+      
+      text-center 
+      font-semibold 
+      
+      cursor-pointer"
+    >Notify Me</p>
+  </div>          
+</template>
