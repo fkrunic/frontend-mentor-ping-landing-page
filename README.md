00f97fcfa6c4d07295f19f067b004b1c68a452b3
Filip Krunic <fkrunic9@gmail.com>
2023-09-06 04:23:28 -0700

    project setup

---
+++ README.md
@@ -0,0 +1,2 @@
+# Ping Landing Page
+Implementation of [Ping Landing Page](https://www.frontendmentor.io/challenges/ping-single-column-coming-soon-page-5cadd051fec04111f7b848da) using Vue and TailwindCSS.
---
+++ src/App.vue
@@ -0,0 +1,15 @@
+<script setup lang="ts">
+import { onMounted } from 'vue';
+
+onMounted(() => {
+  document.body.className = ''
+})
+</script>
+
+<template>
+  <div class="flex items-center justify-center min-h-screen">
+    <p class="text-3xl font-bold underline">
+      Hello world!
+    </p>
+  </div>
+</template>
