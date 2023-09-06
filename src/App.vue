8b8aa4a09350dc5eba7da11889601bcfb2593423
Filip Krunic <fkrunic9@gmail.com>
2023-09-06 13:13:01 -0700

    mobile design

--- src/App.vue
+++ src/App.vue
@@ -7,13 +7,17 @@ onMounted(() => {
 </script>
 
 <template>
-  <div class="flex flex-col items-center justify-center min-h-screen">
-    <p>PING.</p>
-    <p>We are launching <span>soon!</span></p>
-    <p>Subscribe and get notified</p>
-    <input type="text" placeholder="Your email address...">
-    <p>Notify Me</p>
+  <div class="flex flex-col items-center justify-start gap-8 min-h-screen m-10">
+    <p class="text-2xl font-bold pt-20">PING.</p>
+    <div class="flex flex-col items-center gap-4">
+      <p class="text-2xl text-gray">We are launching <span class="font-bold text-black">soon!</span></p>
+      <p class="text-sm">Subscribe and get notified</p>
+    </div>
+    <div class="flex flex-col gap-3 items-center w-full">
+      <input class="w-full rounded-full text-sm placeholder-pale-blue outline-none border-[1px] border-solid border-pale-blue p-4 pl-8" type="text" placeholder="Your email address...">            
+      <p class="bg-blue text-white text-sm w-full rounded-full p-4 text-center font-semibold shadow-xl">Notify Me</p>
+    </div>
     <img src="./assets/illustration-dashboard.png">
-    <p>&#169; Copyright Ping. All rights reserved.</p>
+    <p class="absolute bottom-6 w-full text-center text-xs text-gray">&#169; Copyright Ping. All rights reserved.</p>
   </div>
 </template>
--- tailwind.config.js
+++ tailwind.config.js
@@ -17,7 +17,9 @@ export default {
       'pale-blue': 'hsl(223, 100%, 88%)',
       'light-red': 'hsl(354, 100%, 66%)',
       'gray': 'hsl(0, 0%, 59%)',
-      'very-dark-blue': 'hsl(209, 33%, 12%)'
+      'very-dark-blue': 'hsl(209, 33%, 12%)',
+      'black': 'hsl(0, 0%, 0%)',
+      'white': 'hsl(0, 0%, 100%)'
     },
     extend: {},
   },
